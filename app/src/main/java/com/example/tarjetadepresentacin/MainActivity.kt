1002703bf93c2ed8c0f3bb5df1f1efeb81232d48
noobmx <roberto_1320114135@uptecamac.edu.mx>
2023-10-11 11:34:59 -0600

    Commit final

--- app/src/main/java/com/example/tarjetadepresentacin/MainActivity.kt
+++ app/src/main/java/com/example/tarjetadepresentacin/MainActivity.kt
@@ -8,9 +8,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,8 +24,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,13 +52,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun name(modifier: Modifier = Modifier) {
+    val numero = "+525533351022"
     Column(
-        modifier = modifier,
+        modifier = modifier.background(Color.Gray),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         //
         Column {
+
             Image(
                 painter = painterResource(R.drawable.android_logo),
                 contentDescription = null,
@@ -61,25 +70,55 @@ fun name(modifier: Modifier = Modifier) {
                     .padding(10.dp)
                     .align(alignment = Alignment.CenterHorizontally)
                 )
-                Text(
-                    text = "Roberto Mejia Osorio",
-                    textAlign = TextAlign.Center,
-                    fontSize = 40.sp
-                )
-                Text(
-                    text = "Android Desarrollador Extraordinario!",
-                    Modifier
-                        .align(alignment = Alignment.CenterHorizontally),
-                    color = Color.Green,
-                    )
-            }
-        Column(
-            modifier = modifier,
+            Text(
+                text = stringResource(R.string.nombre_text),
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp
+            )
+            Text(
+                text = stringResource(R.string.enunciado_text),
+                Modifier
+                    .align(alignment = Alignment.CenterHorizontally),
+                color = Color.Green,
+            )
+        }
+    }
 
-        ) {
-                Row {
-
-                }
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            //
+            Icon(
+                Icons.Default.Call,
+                contentDescription = null,
+                tint = Color.Blue
+            )
+            Text(text = numero,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Spacer(Modifier.height(10.dp))
+        Row {
+            Icon(
+                Icons.Default.Share,
+                contentDescription = null,
+                tint = Color.Blue
+            )
+            Text(text = stringResource(R.string.red_social_text),
+                fontWeight = FontWeight.Bold)
+        }
+        Spacer(Modifier.height(10.dp))
+        Row {
+            Icon(
+                Icons.Default.Email,
+                contentDescription = null,
+                tint = Color.Blue
+            )
+            Text(text = stringResource(R.string.correo_text),
+                fontWeight = FontWeight.Bold)
         }
     }
 }
